e8f4cd4eb4bcd5d494dce3e240e302c7d8e1292a
Mirza Raiyan Ahmed <72164519+Mirza-Raiyan-Ahmed@users.noreply.github.com>
2024-12-31 13:04:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ closeButton.addEventListener('click', () => {
     checkbox.checked = false; // Uncheck the checkbox to hide the modal
     localStorage.removeItem('showModal');
 
-    
+    const modal = document.querySelector('.tour-modal');
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        checkbox.checked = false;
 });
 
 </script>

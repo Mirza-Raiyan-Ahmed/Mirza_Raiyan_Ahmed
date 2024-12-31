be06e35b65324e5d94faee4dc6951e8df782a9a1
Mirza Raiyan Ahmed <72164519+Mirza-Raiyan-Ahmed@users.noreply.github.com>
2024-12-31 12:57:00 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const closeButton = document.querySelector('.close-btn');
+const checkbox = document.querySelector('.tour-modal-checkbox');
+
+closeButton.addEventListener('click', () => {
+    checkbox.checked = false; // Uncheck the checkbox to hide the modal
+});
